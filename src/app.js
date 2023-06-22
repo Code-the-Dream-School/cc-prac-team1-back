77328d36f46c9a5235115744bcbe83a14c742345
JenMcD-star <jenniferannvancura@gmail.com>
2023-06-21 21:20:13 -0500

    chai-testing

--- src/app.js
+++ src/app.js
@@ -37,9 +37,18 @@ const connectDB = require("./db/connect");
 const petsRouter = require("./routes/pet");
 const authRouter = require("./routes/auth");
 
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 // Routes
 app.use("/api/v1/pets", petsRouter);
 app.use("/api/v1/auth", authRouter);
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // Sets up the port number for the Express application to listen on
 const port = process.env.PORT || 5005;
@@ -57,3 +66,5 @@ const start = async () => {
 };
 
 start();
+
+module.exports = { app }
